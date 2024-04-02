00bfce50c28b15bcb4d55dae35301c7d9caedb8d
ahmadsaif7456@gmail.com <ahmadsaif7456@gmail.com>
2024-04-03 03:38:37 +0700

    ok

--- coba.py
+++ coba.py
@@ -1,9 +1,25 @@
-bb = int(input('Berat barang : '))
-jp = int(input('jarak pengiriman : '))
-bp = bb * 2000
-km = jp * 500
-bj = bp + km
+namaKaryawan = input('Masukkan Nama : ')
+Jabatan = input('Jabatan [Penyusun buku/Admin]: ').title()
+kh = input('Harian [Y/T] ? ').upper()
 
-print(f'Berat barang :{bb}')
-print(f'Jarak pengiriman :{jp}')
-print(f'Biaya :{bj}')
+if kh == "Y" :
+    gb = 50000 if Jabatan == "Penyusun buku" else 100000 if Jabatan == "Admin" else 0
+    kp = int(input('Berapa Hari Kerja : '))
+    tg = gb * kp
+elif kh == "T" :
+    gb = 1500000 if Jabatan == "Penyusun buku" else 3000000 if Jabatan == "Admin" else 0
+    tg = gb
+else :
+    print('Invalid')
+    exit() 
+
+tugas = "Merapihkan dan mengatur buku di Toko" if Jabatan == "Penyusun buku" else "Mengelola pesanan dan inventaris" if Jabatan == "Admin" else 0
+pajak = 0.05 * tg
+tp = tg - pajak
+
+print('-' * 30) 
+print(f'Nama : {namaKaryawan}')
+print(f'Jabatan : {Jabatan}')
+print(f'Tugasnya : {tugas}')
+print(f'Pajak : {pajak:,}')
+print(f'Total pendapatan : {tp:,}') 
