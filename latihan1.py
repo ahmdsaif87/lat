3dbbc04b75918a944b59102afbf6edd48dc24eaa
ahmdsaif87 <ahmadsaif7456@gmail.com>
2024-09-03 23:03:30 +0700

    okokokok

--- latihan1.py
+++ latihan1.py
@@ -2,12 +2,11 @@ Judul_buku = 'Aku Soeorang Kapiten'
 Tahun_terbit = 2001
 Harga = 25.000
 Penulis = 'Fadil'
-
-
 Ketersediaan = True
 List_Informasi = [Judul_buku, Tahun_terbit, Harga, Penulis, Ketersediaan]
 print(List_Informasi)
 print('Makanan putra : adalah lazuardi')
+
 
 
 # haloooo
